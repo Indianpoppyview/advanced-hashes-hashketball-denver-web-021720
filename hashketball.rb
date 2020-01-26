7e25ac8ec22a50d545dc27431f5d77e1e8172e6c
David Montoya <davidcmontoya@yahoo.com>
2020-01-26 22:19:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,46 @@
 def game_hash{
-  home:{
+  home:{ team_name: "Brooklyn Nets", 
+         colors: %w[Black White],
+         players: [
+           { player_name: "Alan Anderson",
+              number: 0,
+              shoe: 16,
+              points: 22,
+              rebounds: 12,
+              assists: 12,
+              steals: 3,
+              blocks: 1,
+              slam_dunks: 1},
+            { player_name: "Reggie Evans",
+              number:
+              shoe:
+              points:
+              rebounds:
+              assists:
+              steals:
+              blocks:
+                   { player_name: "Brook Lopez",
+              number: 0,
+              shoe: 16,
+              points: 22,
+              rebounds: 12,
+              assists: 12,
+              steals: 3,
+              blocks: 1,
+              slam_dunks: 1},
+            { player_name: "Mason Plumlee",
+              number:
+              shoe:
+              points:
+              rebounds:
+              assists:
+              steals:
+              blocks:
+              slam_dunks:},      slam_dunks:},
+              
+              
+              
+              ]
     
   }
 }
