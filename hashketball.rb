24bbf46cd5b20ef092948df51c7dad8148c000cb
David Montoya <davidcmontoya@yahoo.com>
2020-01-26 23:26:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,8 +126,14 @@ def shoe_size(find_player_name)
   end
 end
 
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    returmn game_hash[place][:colors] if team[:team_name] == team_name
+  end
+end
 
-
+def team_names
+  
 
 
 
